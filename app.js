69f4ec4b9130b95263d52cf8acc9eaae570f9fb7
Cecilia <pereirajardimmariacecilia@gmail.com>
2026-02-16 11:10:28 -0300

    fazendo mais adições no html e iniciando o js

---
+++ app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+
+function selecionarCurso(){
+    const url = `https://lion-school-phbo.onrender.com/cursos`
+
+    const container = document.getElementById('container')
+    
+    // const divEsquerda = document.createElement('div')
+    // const h1 = document.createElement('h1')
+    // const fotoDispositivos = document.createElement('img')
+    // const fotoEstudante = document.createElement('img')
+
+    // h1.textContent(`Escolha um curso para gerenciar`)
+    // fotoDispositivos.src = `./img/devices.png`
+
+    // divEsquerda.appendChild(h1)
+    // divEsquerda.appendChild(fotoDispositivos)
+    // divEsquerda.appendChild(fotoEstudante)
+    // container.appendChild(divEsquerda)
+
+
+
+}
